620b24361ac656266086f4de4efa5604cd09515f
KasK0109 <kaaresb@proton.me>
2025-02-14 12:17:40 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace P2Projekt
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
